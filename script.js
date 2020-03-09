74f502d236026af87e20f3fd18b8032932d5af03
Joris ter Wolbeek <21074092@talnet.nl>
2020-03-09 14:58:00 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var pi = 3.14;
-var diameter;
+const pi = 3.14;
 
 function bereken(){
+    let diameter;
     diameter = document.getElementById("diameter").value;
-    antwoord = diameter*pi
+    antwoord = diameter*diameter*pi*0.25
     document.getElementById("antwoord").innerHTML = antwoord;
 }
